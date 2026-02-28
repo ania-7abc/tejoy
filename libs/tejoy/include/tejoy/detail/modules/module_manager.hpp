79b08c7572a91b1a9897de318a07e29d91131bb8
Ania Baykina <ania_7@inbox.ru>
2026-02-28 21:33:56 +0300

    Create Node, Module, ModuleManager

---
+++ libs/tejoy/include/tejoy/detail/modules/module_manager.hpp
@@ -0,0 +1,88 @@
+// MIT License
+//
+// Copyright (c) 2026 Anya Baykina Dmitrievna
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// module_manager.hpp
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+#include <typeindex>
+#include "module.hpp"
+
+namespace tejoy::detail::modules
+{
+
+    class ModuleManager
+    {
+    public:
+        explicit ModuleManager(event_system::EventBus &bus);
+
+        template <typename T, typename... Args>
+        T &createModule(Args &&...args)
+        {
+            static_assert(std::is_base_of_v<Module, T>, "T must be derived from Module");
+            auto module = std::make_shared<T>(bus_, std::forward<Args>(args)...);
+            auto it = modules_.find(std::type_index(typeid(T)));
+            if (it != modules_.end())
+                throw std::runtime_error("Module of this type already exists");
+            modules_[std::type_index(typeid(T))] = module;
+            return *module;
+        }
+
+        template <typename T>
+        T &getModule()
+        {
+            auto it = modules_.find(std::type_index(typeid(T)));
+            if (it == modules_.end())
+                throw std::runtime_error("Module not found");
+            return *std::dynamic_pointer_cast<T>(it->second);
+        }
+
+        template <typename T>
+        void restartModule()
+        {
+            auto &mod = getModule<T>();
+            mod.onStop();
+            mod.onStart();
+        }
+
+        template <typename T, typename... Args>
+        void recreateModule(Args &&...args)
+        {
+            auto it = modules_.find(std::type_index(typeid(T)));
+            if (it != modules_.end())
+            {
+                it->second->onStop();
+                modules_.erase(it);
+            }
+            createModule<T>(std::forward<Args>(args)...);
+        }
+
+        void startAll();
+        void stopAll();
+
+    private:
+        const event_system::EventBus &bus_;
+        std::unordered_map<std::type_index, std::shared_ptr<Module>> modules_;
+    };
+
+} // namespace tejoy
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,3 @@
-// MIT License
-// 
-// Copyright (c) 2026 Anya Baykina Dmitrievna
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 // main.cpp
 #include <iostream>
 #include <atomic>
@@ -27,20 +5,10 @@
 #include <queue>
 #include <unistd.h>
 
-#include <nlohmann/json.hpp>
+#include <boost/asio.hpp>
 
+#include "tejoy/node.hpp"
 #include "config.hpp"
-
-// Self-Write libs
-#include <secret_box.hpp>
-#include <udp.hpp>
-#include <base64.hpp>
-#include <simple_io.hpp>
-#include <storage.hpp>
-
-using namespace nlohmann;
-
-Storage storage(STORAGE_DIR);
 
 static std::atomic<bool> need_stop = false;
 extern "C" void handle_signal(int signal)
@@ -55,15 +23,17 @@ extern "C" void handle_signal(int signal)
 int main()
 {
   std::signal(SIGINT, handle_signal);
-
-  storage.load();
   std::cout << "Press Ctrl+C to exit" << std::endl;
+
+  boost::asio::io_context io_context;
+  tejoy::Node node(io_context, "data");
 
   while (!need_stop)
   {
     // Work
   }
 
-  storage.save();
+  io_context.stop();
+
   return 0;
 }
