779be2020975554a2d0b2c58b3059cce3642591b
Ania Baykina <ania_7@inbox.ru>
2026-03-01 15:03:34 +0300

    test(main): update example to demonstrate request-response and UpdateManager

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 
 #include <tejoy/node.hpp>
 #include <tejoy/events/detail/packet_events.hpp>
+#include <tejoy/events/data_request.hpp>
 #include "config.hpp"
+#include <future>
 
 static std::atomic<bool> need_stop = false;
 extern "C" void handle_signal(int signal)
@@ -24,12 +26,21 @@ int main()
 
   tejoy::Node node("data", PORT);
 
-  auto sub = node.get_event_bus().make_subscriber<tejoy::events::detail::PacketReceived>(
+  auto sub1 = node.get_event_bus().make_subscriber<tejoy::events::detail::PacketReceived>(
       [](auto &e)
       {
         std::cout << "Packet with text \"" << e.message << "\" from " << e.ip << ":" << e.port << std::endl;
       });
-  node.get_event_bus().publish<tejoy::events::detail::SendPacketRequest>(sub.get(), "Hello, World!", "127.0.0.1", PORT);
+  auto sub2 = node.get_event_bus().make_subscriber<tejoy::events::detail::UpdateReceived>(
+      [](auto &e)
+      {
+        std::cout << "Packet with text \"" << e.update << "\" from " << e.from.ip << ":" << e.from.port << std::endl;
+      });
+  std::promise<tejoy::User> promise;
+  std::future<tejoy::User> fut = promise.get_future();
+  node.get_event_bus().publish<tejoy::events::RequestI>(nullptr, promise);
+  tejoy::User i = fut.get();
+  node.get_event_bus().publish<tejoy::events::detail::SendUpdateRequest>(nullptr, nlohmann::json{{"text", "Hello, World!"}} /* update */, i /* to */);
 
   while (!need_stop)
   {
