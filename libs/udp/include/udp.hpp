8163da1f1cf5e071ff4cd538660636b0eaf6de14
Ania Baykina <ania_7@inbox.ru>
2026-03-01 10:10:59 +0300

    feat(udp): enhance threading model with work_guard and explicit stop()

--- libs/udp/include/udp.hpp
+++ libs/udp/include/udp.hpp
@@ -1,17 +1,17 @@
 // MIT License
-// 
+//
 // Copyright (c) 2026 Anya Baykina Dmitrievna
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,16 +37,19 @@ public:
     UDP(uint16_t port);
     ~UDP();
     void start(Callback callback);
+    void stop();
     void send(const std::string &msg, const std::string &ip, uint16_t port);
 
 private:
     void receive();
 
     boost::asio::io_context io_;
+    boost::asio::executor_work_guard<boost::asio::io_context::executor_type> work_guard_;
+    std::thread thread_;
+
     boost::asio::ip::udp::socket socket_;
     boost::asio::ip::udp::endpoint sender_;
     std::array<char, 1 << 16> buffer_;
-    std::thread thread_;
     Callback callback_;
     bool running_;
 };
--- libs/udp/src/udp.cpp
+++ libs/udp/src/udp.cpp
@@ -1,17 +1,17 @@
 // MIT License
-// 
+//
 // Copyright (c) 2026 Anya Baykina Dmitrievna
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,30 +27,39 @@
 #include <cstdint>
 #include <functional>
 #include <thread>
+#include <exception>
 #include <string>
 #include <array>
+#include <iostream>
 
 UDP::UDP(uint16_t port)
-    : io_(), socket_(io_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)),
+    : io_(), work_guard_(boost::asio::make_work_guard(io_)), thread_([this]()
+                                                                     { io_.run(); }),
+      socket_(io_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port)),
       running_(true)
 {
-
-    thread_ = std::thread([this]()
-                         { io_.run(); });
 }
 
 UDP::~UDP()
 {
-    running_ = false;
-    io_.stop();
-    if (thread_.joinable())
-        thread_.join();
+    stop();
 }
 
 void UDP::start(Callback callback)
 {
+    if (!running_)
+        throw std::runtime_error("UDP is not running");
     this->callback_ = callback;
     receive();
+}
+
+void UDP::stop()
+{
+    running_ = false;
+    work_guard_.reset();
+    io_.stop();
+    if (thread_.joinable())
+        thread_.join();
 }
 
 void UDP::send(const std::string &msg, const std::string &ip, uint16_t port)
@@ -59,7 +68,7 @@ void UDP::send(const std::string &msg, const std::string &ip, uint16_t port)
         boost::asio::ip::make_address(ip), port);
 
     socket_.async_send_to(boost::asio::buffer(msg), ep,
-                         [](auto, auto) {});
+                          [this](auto ec, size_t) {});
 }
 
 void UDP::receive()
@@ -68,15 +77,15 @@ void UDP::receive()
         return;
 
     socket_.async_receive_from(boost::asio::buffer(buffer_), sender_,
-                              [this](auto ec, size_t bytes)
-                              {
-                                  if (!ec && bytes > 0 && callback_)
-                                  {
-                                      callback_(std::string(buffer_.data(), bytes),
-                                               sender_.address().to_string(),
-                                               sender_.port());
-                                  }
-                                  if (running_)
-                                      receive();
-                              });
+                               [this](auto ec, size_t bytes)
+                               {
+                                   if (!ec && bytes > 0 && callback_)
+                                   {
+                                       callback_(std::string(buffer_.data(), bytes),
+                                                 sender_.address().to_string(),
+                                                 sender_.port());
+                                   }
+                                   if (running_)
+                                       receive();
+                               });
 }
