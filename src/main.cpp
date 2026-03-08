2cbe39e18233067205ef680eb7d13d421604b8a4
Ania Baykina <ania_7@inbox.ru>
2026-03-08 09:10:53 +0300

    refactor(main): modernize code style
    - Use trailing return for main.
    - Replace std::endl with '\n'.
    - Use auto where beneficial.
    - Improve variable naming.

--- src/main.cpp
+++ src/main.cpp
@@ -8,51 +8,47 @@
 #include <iostream>
 
 #include <tejoy/events/data_requests.hpp>
+#include <tejoy/events/discovery.hpp>
 #include <tejoy/events/message.hpp>
 #include <tejoy/events/updates.hpp>
-#include <tejoy/events/discovery.hpp>
 #include <tejoy/node.hpp>
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static std::atomic<bool> need_stop = false;
 extern "C" void handle_signal(int signal)
 {
-  if (signal == SIGINT)
-  {
-    std::cout << "\rShutting down..." << std::endl;
-    need_stop = true;
-  }
+    if (signal == SIGINT)
+    {
+        std::cout << "\rShutting down..." << '\n';
+        need_stop = true;
+    }
 }
 
-int main()
+auto main() -> int
 {
-  std::signal(SIGINT, handle_signal);
-  std::cout << "Press Ctrl+C to exit" << std::endl;
+    std::signal(SIGINT, handle_signal);
+    std::cout << "Press Ctrl+C to exit" << '\n';
 
-  tejoy::Node node("data", PORT);
+    tejoy::Node node("data", PORT);
 
-  auto sub1 = node.get_event_bus().make_subscriber<tejoy::events::MessageUpdateReceived>(
-      [](auto &e)
-      {
-        std::cout << "\"" << e.text << "\" from " << e.from.ip << ":" << e.from.port << std::endl;
-      });
-  auto sub2 = node.get_event_bus().make_subscriber<tejoy::events::DiscoveredNewNode>(
-      [](auto &e)
-      {
-        std::cout << "Found " << e.node.ip << std::endl;
-      });
+    auto sub1 = node.get_event_bus().make_subscriber<tejoy::events::MessageUpdateReceived>([](auto &event) {
+        std::cout << "\"" << event.text << "\" from " << event.from.ip << ":" << event.from.port << std::endl;
+    });
+    auto sub2 = node.get_event_bus().make_subscriber<tejoy::events::DiscoveredNewNode>(
+        [](auto &event) { std::cout << "Found " << event.node.ip << std::endl; });
 
-  std::promise<tejoy::User> promise;
-  std::future<tejoy::User> fut = promise.get_future();
-  node.get_event_bus().publish<tejoy::events::RequestI>(nullptr, promise);
-  tejoy::User i = fut.get();
-  
-  node.get_event_bus().publish<tejoy::events::SendMessageRequest>(nullptr, "Hello, World!", i);
-  node.get_event_bus().publish<tejoy::events::SendMessageRequest>(nullptr, "Hello, World!", i);
+    std::promise<tejoy::User> promise;
+    std::future<tejoy::User> fut = promise.get_future();
+    node.get_event_bus().publish<tejoy::events::RequestI>(nullptr, promise);
+    tejoy::User recipient = fut.get();
 
-  while (!need_stop)
-  {
-    // Work
-  }
+    node.get_event_bus().publish<tejoy::events::SendMessageRequest>(nullptr, "Hello, World!", recipient);
+    node.get_event_bus().publish<tejoy::events::SendMessageRequest>(nullptr, "Hello, World!", recipient);
 
-  return 0;
+    while (!need_stop)
+    {
+        // Work
+    }
+
+    return 0;
 }
