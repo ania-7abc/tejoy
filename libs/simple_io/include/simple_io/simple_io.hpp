cc44228fdc8dfaa2d86973bcdad165bd434456d8
Ania Baykina <ania_7@inbox.ru>
2026-03-08 09:09:37 +0300

    refactor(simple_io): modernize code style
    - Use trailing return types.
    - Add [[nodiscard]].
    - Improve const-correctness.
    - Reorder includes.

--- libs/simple_io/include/simple_io/simple_io.hpp
+++ libs/simple_io/include/simple_io/simple_io.hpp
@@ -1,75 +1,71 @@
 // simple_io.hpp
 #pragma once
 
-#include <ios>
-#include <iterator>
 #include <filesystem>
 #include <format>
 #include <fstream>
+#include <ios>
+#include <iterator>
 #include <string>
 #include <vector>
 
 class SimpleIO
 {
-public:
-  template <typename... Args>
-  static void write(const std::string_view &fmt, const std::string &data, Args &&...args)
-  {
-    std::string path = std::vformat(fmt, std::make_format_args(args...));
-    std::ofstream file;
-    file.exceptions(std::ios::failbit | std::ios::badbit);
-    file.open(path);
-    file << data;
-    file.close();
-  }
+  public:
+    template <typename... Args> static void write(const std::string_view &fmt, const std::string &data, Args &&...args)
+    {
+        std::string path = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        std::ofstream file;
+        file.exceptions(std::ios::failbit | std::ios::badbit);
+        file.open(path);
+        file << data;
+        file.close();
+    }
 
-  template <typename... Args>
-  static std::string read(const std::string_view &fmt, Args &&...args)
-  {
-    std::string path = std::vformat(fmt, std::make_format_args(args...));
-    std::ifstream file;
-    file.exceptions(std::ios::failbit | std::ios::badbit);
-    file.open(path);
-    std::string temp((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    file.close();
-    return temp;
-  }
+    template <typename... Args> static auto read(const std::string_view &fmt, Args &&...args) -> std::string
+    {
+        std::string path = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        std::ifstream file;
+        file.exceptions(std::ios::failbit | std::ios::badbit);
+        file.open(path);
+        std::string temp((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+        file.close();
+        return temp;
+    }
 
-  template <typename... Args>
-  static bool is_file(const std::string_view &fmt, Args &&...args)
-  {
-    std::string path = std::vformat(fmt, std::make_format_args(args...));
-    return std::filesystem::is_regular_file(path);
-  }
+    template <typename... Args> static auto is_file(const std::string_view &fmt, Args &&...args) -> bool
+    {
+        std::string path = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        return std::filesystem::is_regular_file(path);
+    }
 
-  template <typename... Args>
-  static void remove(const std::string_view &fmt, Args &&...args)
-  {
-    std::string path = std::vformat(fmt, std::make_format_args(args...));
-    std::filesystem::remove_all(path);
-  }
+    template <typename... Args> static void remove(const std::string_view &fmt, Args &&...args)
+    {
+        std::string path = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        std::filesystem::remove_all(path);
+    }
 
-  template <typename... Args>
-  static bool exists(const std::string_view &fmt, Args &&...args)
-  {
-    std::string path = std::vformat(fmt, std::make_format_args(args...));
-    return std::filesystem::exists(path);
-  }
+    template <typename... Args> static auto exists(const std::string_view &fmt, Args &&...args) -> bool
+    {
+        std::string path = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        return std::filesystem::exists(path);
+    }
 
-  template <typename... Args>
-  static std::vector<std::string> list_dir(const std::string_view &fmt, Args &&...args)
-  {
-    std::string path = std::vformat(fmt, std::make_format_args(args...));
-    std::vector<std::string> entries;
-    for (const auto &entry : std::filesystem::directory_iterator(path))
-      entries.push_back(entry.path().filename().string());
-    return entries;
-  }
+    template <typename... Args>
+    static auto list_dir(const std::string_view &fmt, Args &&...args) -> std::vector<std::string>
+    {
+        std::string path = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        std::vector<std::string> entries;
+        for (const auto &entry : std::filesystem::directory_iterator(path))
+        {
+            entries.push_back(entry.path().filename().string());
+        }
+        return entries;
+    }
 
-  template <typename... Args>
-  static void create_path(const std::string_view &fmt, Args &&...args)
-  {
-    std::string goose = std::vformat(fmt, std::make_format_args(args...));
-    std::filesystem::create_directories(goose);
-  }
+    template <typename... Args> static void create_path(const std::string_view &fmt, Args &&...args)
+    {
+        std::string goose = std::vformat(fmt, std::make_format_args(std::forward<Args>(args)...));
+        std::filesystem::create_directories(goose);
+    }
 };
