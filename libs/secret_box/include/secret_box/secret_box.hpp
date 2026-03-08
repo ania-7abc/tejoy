8f9248f2f7bdd7c800b136f979017affec2f139b
Ania Baykina <ania_7@inbox.ru>
2026-03-08 09:09:28 +0300

    refactor(secret_box): modernize code style
    - Use trailing return types.
    - Add [[nodiscard]].
    - Mark init_sodium as static.
    - Improve const-correctness.
    - Add NOLINT comments for pointer arithmetic where necessary.

--- libs/secret_box/include/secret_box/secret_box.hpp
+++ libs/secret_box/include/secret_box/secret_box.hpp
@@ -1,30 +1,32 @@
 // secret_box.hpp
 #pragma once
+#include <sodium.h>
 #include <stdexcept>
 #include <vector>
-#include <sodium.h>
 
 class SecretBox
 {
-private:
-  std::vector<uint8_t> secret_key_, public_key_;
-  bool has_secret_, has_public_;
+  private:
+    std::vector<uint8_t> secret_key_, public_key_;
+    bool has_secret_, has_public_;
 
-  void init_sodium();
+    static void init_sodium();
 
-public:
-  SecretBox(bool init_keys = true);
-  SecretBox(const std::vector<uint8_t> &public_key);
-  SecretBox(const std::vector<uint8_t> &secret_key, const std::vector<uint8_t> &public_key);
+  public:
+    explicit SecretBox(bool gen_keys = true);
+    explicit SecretBox(const std::vector<uint8_t> &public_key);
+    SecretBox(const std::vector<uint8_t> &secret_key, const std::vector<uint8_t> &public_key);
 
-  const std::vector<uint8_t> &get_public_key() const;
-  const std::vector<uint8_t> &get_secret_key() const;
-  bool has_secret_key() const;
-  bool has_public_key() const;
+    [[nodiscard]] auto get_public_key() const -> const std::vector<uint8_t> &;
+    [[nodiscard]] auto get_secret_key() const -> const std::vector<uint8_t> &;
+    [[nodiscard]] auto has_secret_key() const -> bool;
+    [[nodiscard]] auto has_public_key() const -> bool;
 
-  std::vector<uint8_t> encrypt(const std::string &text, const std::vector<uint8_t> &recipient_public_key) const;
-  std::vector<uint8_t> encrypt(const std::string &text, const SecretBox &recipient) const;
+    [[nodiscard]] auto encrypt(const std::string &text,
+                               const std::vector<uint8_t> &recipient_public_key) const -> std::vector<uint8_t>;
+    [[nodiscard]] auto encrypt(const std::string &text, const SecretBox &recipient) const -> std::vector<uint8_t>;
 
-  std::string decrypt(const std::vector<uint8_t> &data, const std::vector<uint8_t> &sender_public_key) const;
-  std::string decrypt(const std::vector<uint8_t> &data, const SecretBox &sender) const;
+    [[nodiscard]] auto decrypt(const std::vector<uint8_t> &data,
+                               const std::vector<uint8_t> &sender_public_key) const -> std::string;
+    [[nodiscard]] auto decrypt(const std::vector<uint8_t> &data, const SecretBox &sender) const -> std::string;
 };
