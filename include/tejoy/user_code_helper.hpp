4fae6ca431e6f036347d060d84e71248891059ad
Ania Baykina <ania_7@inbox.ru>
2026-04-01 11:39:25 +0300

    feat(user-code): add public API for sending messages and event callbacks

--- include/tejoy/user_code_helper.hpp
+++ include/tejoy/user_code_helper.hpp
@@ -1,19 +1,36 @@
 // user_code_helper.hpp
 #pragma once
 #include <event_system/subscriber.hpp>
+#include <tejoy/events/errors.hpp>
+#include <tejoy/user.hpp>
 
 namespace tejoy
 {
 
 class UserCodeHelper : public event_system::Subscriber
 {
+  public:
+    void send_message(const std::string &text, const User &recipient);
+    void on_message(std::function<void(const std::string &, const User &)> handler);
+    void on_discovered_node(std::function<void(const tejoy::User &)> handler);
+    void on_invalid_update_error(std::function<void(const events::InvalidUpdateError &)> handler);
+    void on_update_send_error(std::function<void(const events::UpdateSendError &)> handler);
+
+    std::string get_ip();
+    uint16_t get_port();
+    std::string get_discovery_ip();
+    User get_i();
+
   private:
     friend class Node;
     using Subscriber::Subscriber;
 
     void start();
 
-  public:
+    std::function<void(const std::string &, const User &)> on_message_handler = [](auto, auto) {};
+    std::function<void(const tejoy::User &)> on_discovered_node_handler = [](auto) {};
+    std::function<void(const events::InvalidUpdateError &)> on_invalid_update_error_handler = [](auto) {};
+    std::function<void(const events::UpdateSendError &)> on_update_send_error_handler = [](auto) {};
 };
 
 } // namespace tejoy
--- src/user_code_helper.cpp
+++ src/user_code_helper.cpp
@@ -1,5 +1,7 @@
 // user_code_helper.cpp
+#include <tejoy/events/data_requests.hpp>
 #include <tejoy/events/detail/updates.hpp>
+#include <tejoy/events/discovery.hpp>
 #include <tejoy/events/message.hpp>
 #include <tejoy/user_code_helper.hpp>
 
@@ -20,6 +22,68 @@ void UserCodeHelper::start()
                                              event.sender);
         }
     });
+
+    subscribe<events::MessageReceived>([this](auto &event) { on_message_handler(event.text, event.sender); });
+    subscribe<events::DiscoveredNewNode>([this](auto &event) { on_discovered_node_handler(event.node); });
+    subscribe<events::InvalidUpdateError>([this](auto &event) { on_invalid_update_error_handler(event); });
+    subscribe<events::UpdateSendError>([this](auto &event) { on_update_send_error_handler(event); });
+}
+
+std::string UserCodeHelper::get_ip()
+{
+    auto promise = std::promise<std::string>();
+    auto fut = promise.get_future();
+    publish<tejoy::events::RequestIp>(promise);
+    return fut.get();
+}
+
+uint16_t UserCodeHelper::get_port()
+{
+    auto promise = std::promise<uint16_t>();
+    auto fut = promise.get_future();
+    publish<tejoy::events::RequestPort>(promise);
+    return fut.get();
+}
+
+std::string UserCodeHelper::get_discovery_ip()
+{
+    auto promise = std::promise<std::string>();
+    auto fut = promise.get_future();
+    publish<tejoy::events::RequestDiscoveryIp>(promise);
+    return fut.get();
+}
+
+User UserCodeHelper::get_i()
+{
+    auto promise = std::promise<tejoy::User>();
+    auto fut = promise.get_future();
+    publish<tejoy::events::RequestI>(promise);
+    return fut.get();
+}
+
+void UserCodeHelper::send_message(const std::string &text, const User &recipient)
+{
+    publish<events::SendMessageRequest>(text, recipient);
+}
+
+void UserCodeHelper::on_message(std::function<void(const std::string &, const User &)> handler)
+{
+    on_message_handler = std::move(handler);
+}
+
+void UserCodeHelper::on_discovered_node(std::function<void(const tejoy::User &)> handler)
+{
+    on_discovered_node_handler = std::move(handler);
+}
+
+void UserCodeHelper::on_invalid_update_error(std::function<void(const events::InvalidUpdateError &)> handler)
+{
+    on_invalid_update_error_handler = std::move(handler);
+}
+
+void UserCodeHelper::on_update_send_error(std::function<void(const events::UpdateSendError &)> handler)
+{
+    on_update_send_error_handler = std::move(handler);
 }
 
 } // namespace tejoy
