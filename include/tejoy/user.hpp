afc92a912bf838b77a0788b2f43d8ae49869ee7c
Ania Baykina <ania_7@inbox.ru>
2026-04-02 10:03:12 +0300

    refactor: move User JSON serialization to separate source file
    Move to_json and from_json implementations from user.hpp to user.cpp
    to reduce header dependencies.

--- include/tejoy/user.hpp
+++ include/tejoy/user.hpp
@@ -2,11 +2,9 @@
 #pragma once
 
 #include <cstdint>
-#include <string>
-
-#include <base64/base64.hpp>
 #include <nlohmann/json.hpp>
 #include <secret_box/secret_box.hpp>
+#include <string>
 
 namespace tejoy
 {
@@ -18,41 +16,7 @@ struct User
     uint16_t port{};
 };
 
-inline void to_json(nlohmann::json &json, const User &user)
-{
-    json = nlohmann::json{};
-    json["ip"] = user.ip;
-    json["port"] = user.port;
-    if (user.box.has_secret_key())
-    {
-        json["key"] = Base64::encode(user.box.get_secret_key());
-    }
-    if (user.box.has_public_key())
-    {
-        json["id"] = Base64::encode(user.box.get_public_key());
-    }
-}
-
-inline void from_json(const nlohmann::json &json, User &user)
-{
-    json.at("ip").get_to(user.ip);
-    json.at("port").get_to(user.port);
-    if (json.contains("id") && json.contains("key"))
-    {
-        user.box = SecretBox(Base64::decode(json["key"]), Base64::decode(json["id"]));
-    }
-    else if (json.contains("id"))
-    {
-        user.box = SecretBox(Base64::decode(json["id"]));
-    }
-    else if (json.contains("key"))
-    {
-        throw std::runtime_error("SecretBox must not contain only the private key");
-    }
-    else
-    {
-        user.box = SecretBox(false);
-    }
-}
+void to_json(nlohmann::json &json, const User &user);
+void from_json(const nlohmann::json &json, User &user);
 
 } // namespace tejoy
